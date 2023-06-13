3c1ef701fbe3b3baff9583443121449d27c5b1d8
Michał Kaczmarowski <michal@movi.ai>
2023-06-13 20:58:44 +0200

    renaming angular and .NET projects

--- src/ExoticAuctionHouseAdmin-API/Controllers/CarController.cs
+++ src/ExoticAuctionHouseAdmin-API/Controllers/CarController.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace KomisSamochodowyAdmin_API.Controllers
+namespace ExoticAuctionHouseAdmin_API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
