30755c137c9fea96dab7ac3c7442fa9a82853f25
Kamil Kot <kamil.kot07@gmail.com>
2023-07-20 22:42:00 +0200

    authorization server

---
+++ src/AuthorizationServer/AuthModels/Enums/Roles.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthModels.Enums
+{
+    public enum Roles
+    {
+        [Description("User")]
+        User,
+        [Description("Admin")]
+        Admin
+    }
+}
---
+++ src/AuthorizationServer/AuthModels/Informations/UserInformation.cs
@@ -0,0 +1,18 @@
+﻿using AuthModels.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthModels.Informations
+{
+    public class UserInformation
+    {
+        public Guid Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public Roles Role { get; set; }
+    }
+}
