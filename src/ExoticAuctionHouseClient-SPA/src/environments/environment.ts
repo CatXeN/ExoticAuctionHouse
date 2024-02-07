b400c4509a1ce4bbae01ce409ba0a880dbdea749
Kamil Kot <kamil.kot07@gmail.com>
2024-02-07 20:29:03 +0100

    Fix words and secure before -1

--- src/ExoticAuctionHouseClient-SPA/src/environments/environment.ts
+++ src/ExoticAuctionHouseClient-SPA/src/environments/environment.ts
@@ -5,3 +5,11 @@ export const environment = {
   authApiUrl: 'https://exoticah.pl:7250',
   auctionServer: 'https://exoticah.pl:7221'
 };
+
+// export const environment = {
+//   production: false,
+//   apiUrl: 'https://localhost:7218',
+//   paymentApiUrl: 'https://localhost:7260',
+//   authApiUrl: 'https://localhost:7250',
+//   auctionServer: 'https://localhost:7221'
+// };
