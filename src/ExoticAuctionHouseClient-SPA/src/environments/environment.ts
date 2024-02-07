82f62681e9774304207e779b39cb5b35dd27963b
Kamil Kot <kamil.kot07@gmail.com>
2024-02-07 20:42:50 +0100

    Test env

--- src/ExoticAuctionHouseClient-SPA/src/environments/environment.ts
+++ src/ExoticAuctionHouseClient-SPA/src/environments/environment.ts
@@ -1,15 +1,7 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://exoticah.pl:7218',
-  paymentApiUrl: 'https://exoticah.pl:7260',
-  authApiUrl: 'https://exoticah.pl:7250',
-  auctionServer: 'https://exoticah.pl:7221'
+  apiUrl: 'https://localhost:7218',
+  paymentApiUrl: 'https://localhost:7260',
+  authApiUrl: 'https://localhost:7250',
+  auctionServer: 'https://localhost:7221'
 };
-
-// export const environment = {
-//   production: false,
-//   apiUrl: 'https://localhost:7218',
-//   paymentApiUrl: 'https://localhost:7260',
-//   authApiUrl: 'https://localhost:7250',
-//   auctionServer: 'https://localhost:7221'
-// };
